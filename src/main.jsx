15a526f51231da79c7ef9222904d946d9aed6008
Jmik Thang <jmikthang5@gmail.com>
2026-06-27 21:42:48 -0400

    google login complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider} from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import App from "./App/App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  </GoogleOAuthProvider>
+  <GoogleOAuthProvider clientId="417912331402-uf5kjddeube7rt3gieb40e21pi3lk1ej.apps.googleusercontent.com">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </GoogleOAuthProvider>,
 );
